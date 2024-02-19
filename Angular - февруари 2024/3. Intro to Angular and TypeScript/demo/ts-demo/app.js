296e50bb9e70123884b53ce7062da178e0e08a24
Todor Krumov <31984842+baito6u@users.noreply.github.com>
2024-02-20 00:21:06 +0200

    Start of Angular

---
+++ Angular - февруари 2024/3. Intro to Angular and TypeScript/demo/ts-demo/app.js
@@ -0,0 +1,7 @@
+var a = 1;
+var text = "hello world";
+var hasCats = true;
+var numArray = [1, 2, 3, 4, 5];
+var numArray2 = [1, 2, 3, 4, 5];
+var strArray = ['a', 'b', 'c'];
+console.log(strArray);
---
+++ Angular - февруари 2024/3. Intro to Angular and TypeScript/demo/ts-demo/app.ts
@@ -0,0 +1,14 @@
+let a: number = 1;
+
+let text: string = "hello world";
+
+let hasCats = true;
+
+let numArray: number[] = [1, 2, 3, 4, 5];
+let numArray2: Array<number> = [1, 2, 3, 4, 5];
+
+let strArray: Array<string> = ['a', 'b', 'c'];
+console.log(strArray);
+
+
+
