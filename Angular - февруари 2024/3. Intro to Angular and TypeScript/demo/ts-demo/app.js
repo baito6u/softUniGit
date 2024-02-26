ded76603ee873bf1797f805c019f697f073aadb9
Todor Krumov <31984842+baito6u@users.noreply.github.com>
2024-02-26 22:13:35 +0200

    Anglar delete
    change to new repo

--- Angular - февруари 2024/3. Intro to Angular and TypeScript/demo/ts-demo/app.js
+++
@@ -1,7 +0,0 @@
-var a = 1;
-var text = "hello world";
-var hasCats = true;
-var numArray = [1, 2, 3, 4, 5];
-var numArray2 = [1, 2, 3, 4, 5];
-var strArray = ['a', 'b', 'c'];
-console.log(strArray);
